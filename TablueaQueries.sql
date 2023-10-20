1b516c1d4ff78de130f743e222862a0181d0d4f6
Hoora Mohseni <129911252+hoora-mohseni@users.noreply.github.com>
2023-10-20 14:53:47 -0400

    Update Tabluea Queries.sql

--- TablueaQueries.sql
+++ TablueaQueries.sql
@@ -16,7 +16,7 @@ where continent is not null
 order by 1,2
 
 -- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
+-- numbers are extremely close so I will keep them - The Second includes "International"  Location
 
 
 --Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
@@ -29,7 +29,7 @@ order by 1,2
 
 -- 2. 
 
--- We take these out as they are not inluded in the above queries and want to stay consistent
+-- I take these out as they are not inluded in the above queries and want to stay consistent
 -- European Union is part of Europe
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
@@ -71,7 +71,7 @@ order by PercentPopulationInfected desc
 
 
 -- Queries I originally had, but excluded some because it created too long of video
--- Here only in case you want to check them out
+-- Here only in case of checking them out
 
 
 -- 1.
@@ -113,7 +113,7 @@ order by 1,2
 
 -- 3.
 
--- We take these out as they are not inluded in the above queries and want to stay consistent
+-- I take these out as they are not inluded in the above queries and want to stay consistent
 -- European Union is part of Europe
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
